84cd97054490ff96f5777aecbe4e19e90ad43d7f
Areliya13 <sharipovatamara@gmail.com>
2022-12-16 11:50:47 +0300

    feat: add books data

---
+++ src/components/dataStructure/Interfaces.ts
@@ -0,0 +1,13 @@
+interface book {
+  id: number,
+  publisher: string,
+  description: string,
+  price: number,
+  title: string,
+  author: string,
+  genre: string,
+  book_image: string[],
+  stock_balance: number,
+}
+
+export {book}
